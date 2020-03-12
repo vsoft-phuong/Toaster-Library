528cf249f2f4ad8d416597a472e8791ace535759
vsoftphuong <vsoftphuong@gmail.com>
2020-03-12 21:38:04 +0700

    Initial commit

---
+++ toasterlibrary/src/main/java/home/study/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,11 @@
+package home.study.toasterlibrary;
+
+import android.content.Context;
+import android.graphics.drawable.Icon;
+import android.widget.Toast;
+
+public class ToasterMessage {
+    public static void toastMessage(Context ctx, String message){
+        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+    }
+}
